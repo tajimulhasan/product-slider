697c7f57328257b37d43cd8664a82b6eeb9b1245
tajimulhasan <tajimulhasan1@gmail.com>
2024-11-01 11:15:57 +0600

    -product-slider-project

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export default function Carousel(){
+    return(
+        <div>
+            <h2 className={styles.tagging}>Hello Tajimul Hasan from carousel, Hi page do you know mee</h2>
+        </div>
+    );
+}
